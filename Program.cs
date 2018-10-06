417c05370c33347c0dbd6ce15c07e6f95c43ddba
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 15:14:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamReaderWriterDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("Stream.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(123456);
+            sw.WriteLine(".NET Batch");
+            sw.WriteLine(false);
+            sw.WriteLine(Math.PI);
+            sw.Flush();
+            fs.Close();
+
+            fs = new FileStream("Stream.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            Console.WriteLine(sr.ReadToEnd());
+            fs.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
